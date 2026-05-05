40bbc563b5d084a0b37f91320395d622b44bb22e
rexbiswas <rexbiswas1@gmail.com>
2026-05-05 17:29:10 +0530

    features

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ import { syncBackups } from './utils/offlineLogger.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, '.env') });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
