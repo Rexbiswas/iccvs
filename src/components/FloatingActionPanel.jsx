3ce9e04f4935c55b25d698b67e54fe15ca602d3f
rexbiswas <rexbiswas1@gmail.com>
2026-05-09 23:45:09 +0530

    features

--- src/components/FloatingActionPanel.jsx
+++ src/components/FloatingActionPanel.jsx
@@ -98,11 +98,17 @@ const FloatingActionPanel = () => {
                                     <BackToTop isFloatingPanel />
                                 </motion.div>
                             )}
+                            {/* Whatsapp is now persistent on all devices but hides when menu is open */}
+                            <motion.div
+                                initial={{ opacity: 0, scale: 0.8 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.8 }}
+                            >
+                                <WhatsappCTA isFloatingPanel />
+                            </motion.div>
                         </div>
                     )}
                 </AnimatePresence>
-                {/* Whatsapp is now persistent on all devices */}
-                <WhatsappCTA isFloatingPanel />
             </div>
 
             {/* Headless AIChatbot for mobile event listening */}
